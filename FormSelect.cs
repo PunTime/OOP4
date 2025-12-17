4b6b99f8b97d00b1b44378ea6034af1d8a4c5199
PunTime <borovikrostislav641@gmail.com>
2025-12-17 11:41:23 +0200

    Add files via upload

---
+++ FormSelect.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP4
+{
+    /* Форма вибору фігури */
+    public class FormSelect : Form
+    {
+        public FormSelect()
+        {
+            Text = "Вибір фігури";
+            Width = 300;
+            Height = 250;
+
+            /* Кнопки вибору */
+            var btnCircle = new Button { Text = "Коло", Top = 30, Width = 200, Left = 40 };
+            var btnSquare = new Button { Text = "Квадрат", Top = 80, Width = 200, Left = 40 };
+            var btnRhomb = new Button { Text = "Ромб", Top = 130, Width = 200, Left = 40 };
+
+            /* Обробники натискання */
+            btnCircle.Click += (_, __) => OpenCanvas("circle");
+            btnSquare.Click += (_, __) => OpenCanvas("square");
+            btnRhomb.Click += (_, __) => OpenCanvas("rhomb");
+
+            Controls.AddRange(new[] { btnCircle, btnSquare, btnRhomb });
+        }
+
+        /* Відкриття форми з малюванням */
+        void OpenCanvas(string type)
+        {
+            Hide();
+            new FormCanvas(type, this).Show();
+        }
+    }
+}
---
+++ Rhomb.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP4
+{
+    /* Клас Ромб — нащадок Figure */
+    public class Rhomb : Figure
+    {
+        /* Довжини діагоналей */
+        private int hor;
+        private int vert;
+
+        public Rhomb(int x, int y, int h, int v, Graphics g, FormCanvas canvas)
+            : base(x, y, g, canvas)
+        {
+            hor = h;
+            vert = v;
+        }
+
+        /* Формуємо вершини ромба */
+        private Point[] Points()
+        {
+            return new Point[]
+            {
+                new Point(x, y - vert / 2),
+                new Point(x + hor / 2, y),
+                new Point(x, y + vert / 2),
+                new Point(x - hor / 2, y)
+            };
+        }
+
+        /* Малювання ромба */
+        public override void DrawBlack()
+        {
+            g.DrawPolygon(Pens.Black, Points());
+        }
+
+        /* Стирання ромба */
+        public override void HideDrawingBackGround()
+        {
+            g.DrawPolygon(canvas.BackPen, Points());
+        }
+    }
+}
